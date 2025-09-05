b38da1f253edf3039fccc783ce2ab2811ceea85a
Van Thanh <lovanthanh34523@email.com>
2025-09-05 22:46:43 +0700

    huhuh

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const port = 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://product-demo-lilac-three.vercel.app/",
     credentials: true,
   })
 );
